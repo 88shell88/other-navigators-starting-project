4a817ed47bc8ab3f1f1030187cb48e2bef7d47d9
Shelley Gill <shelley-gill-88@hotmail.com>
2023-03-07 17:25:07 +0100

    add drawer navigation

--- App.js
+++ App.js
@@ -1,5 +1,21 @@
-import { Text } from 'react-native';
+/** @format */
+
+import { Text } from "react-native";
+import { NavigationContainer } from "@react-navigation/native";
+import { createDrawerNavigator } from "@react-navigation/drawer";
+
+import WelcomeScreen from "./app/screens/WelcomeScreen";
+import UserScreen from "./app/screens/UserScreen";
+
+const Drawer = createDrawerNavigator();
 
 export default function App() {
-  return <Text>Todo...</Text>;
+  return (
+    <NavigationContainer>
+      <Drawer.Navigator initialRouteName="User">
+        <Drawer.Screen name="Welcome" component={WelcomeScreen} />
+        <Drawer.Screen name="User" component={UserScreen} />
+      </Drawer.Navigator>
+    </NavigationContainer>
+  );
 }
